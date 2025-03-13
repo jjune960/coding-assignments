45994f84de0344ed8f6a4cd796c5e8ea9af537ca
jjune960 <jjune960@gmail.com>
2025-03-14 00:16:49 +0900

    feat: 폰켓몬 문제 1차 시도 (미완성)

--- src/programmers/_1845/Solution.java
+++ src/programmers/_1845/Solution.java
@@ -3,6 +3,33 @@ package programmers._1845;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
+
+        int last = nums.length/2;
+
+        int[] count = new int[last];
+
+        //count[] = 0;
+
+        for (int n = 0; n < last; n++) {
+            if (n != 0)
+                count[n] = count[n-1] + 1;
+            else
+                count[n] = 0;
+            for (; count[n] < last - n; count[n]++) {
+
+            }
+        }
+
+
+//        for (count[0] = 0; count[0] < last; count[0]++) {
+//            for (int n = 1; n < last; n++) {
+//                for (count[n] = count[n-1] + 1; count[n] < nums.length - 1; count[n]++) {
+//
+//                }
+//            }
+//
+//        }
+
         return answer;
     }
 }
