320c45b72aecafe746a045117050ab053892e9cf
jjune960 <jjune960@gmail.com>
2025-03-14 12:54:13 +0900

    Create _42576(Marathon retire) library and docs folder and file

---
+++ src/programmers/_42576/Solution.java
@@ -0,0 +1,9 @@
+package programmers._42576;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+
+        return answer;
+    }
+}
