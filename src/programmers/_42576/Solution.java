dc3e6b8663f65dc0debcdb15f6fe6f0bceaf4406
jjune960 <jjune960@gmail.com>
2025-03-14 13:25:26 +0900

    feat: 완주하지 못한 선수 문제 1차 시도 (실패)

--- src/programmers/_42576/Solution.java
+++ src/programmers/_42576/Solution.java
@@ -1,8 +1,22 @@
 package programmers._42576;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+
+        List<String> participantArray = new ArrayList<>();
+        List<String> completionArray = new ArrayList<>();
+
+        participantArray.addAll(Arrays.asList(participant));
+        completionArray.addAll(Arrays.asList(completion));
+
+        participantArray.removeAll(completionArray);
+
+        answer = participantArray.toString();
 
         return answer;
     }
