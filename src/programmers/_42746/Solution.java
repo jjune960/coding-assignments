90799ca8dea6cdbe3952268afbacd5b534999bc7
jjune960 <jjune960@gmail.com>
2025-03-21 23:33:37 +0900

    feat: 가장 큰 수 1차 시도 (실패)

--- src/programmers/_42746/Solution.java
+++ src/programmers/_42746/Solution.java
@@ -4,8 +4,9 @@ class Solution {
     public String solution(int[] numbers) {
         String answer = "";
 
-        int prev_number = 0; // 상위 자릿수의 숫자와 카운트
+        //int prev_number = 0; // 상위 자릿수의 숫자와 카운트
         int prev_count = 0;
+        int[] ansnumbers = new int[numbers.length];
 
         for (int number : numbers) {
             int tempnum = number; // 임시 변수 생성
@@ -18,8 +19,8 @@ class Solution {
             for(int i = 0; i < count; i++) { // 카운트한 개수의 바로 직전 까지만 계산하면
                 tempnum = tempnum / 10;
             }
+            System.out.println(tempnum);
 
-//            if ()
         }
 
         return answer;
