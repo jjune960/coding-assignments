b59743e502ad649fa463b020ea106b7460b0ed87
jjune960 <jjune960@gmail.com>
2025-03-22 12:29:34 +0900

    feat: 가장 큰 수 2차 시도 (3. 0인 경우 처리하기)

--- src/programmers/_42746/Solution.java
+++ src/programmers/_42746/Solution.java
@@ -1,6 +1,7 @@
 package programmers._42746;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public String solution(int[] numbers) {
@@ -11,13 +12,16 @@ class Solution {
             numStrs[i] = String.valueOf(numbers[i]);
         }
 
-        Arrays.sort(numStrs, new Comparator<String>() { // 내림차순 정렬
-            public int compare(String s1, String s2) {
-                return (s2 + s1).compareTo(s1 + s2);
-            }
-        });
+        Arrays.sort(numStrs,
+                new Comparator<String>() { // 내림차순 정렬
+                    public int compare(String s1, String s2) {
+                        return (s2 + s1).compareTo(s1 + s2);
+                    }
+                });
 
-
+        if (numStrs[0].equals("0")) { // 가장 큰 숫자가 0인 경우
+            answer = "0";
+        }
 
         return answer;
     }
