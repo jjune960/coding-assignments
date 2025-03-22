851fad18e2d277c9c5097445dfe6bd395dd9483f
jjune960 <jjune960@gmail.com>
2025-03-22 15:55:07 +0900

    feat: H-index 1차 시도 (2. Integer 배열로 변환하는 방법)

--- src/programmers/_42747/Solution.java
+++ src/programmers/_42747/Solution.java
@@ -1,8 +1,13 @@
 package programmers._42747;
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+
+        Arrays.stream(citations).boxed();
+
         return answer;
     }
 }
