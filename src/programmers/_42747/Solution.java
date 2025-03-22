43d09aaf2f2f65dad62986019acfdb1ea31c1886
jjune960 <jjune960@gmail.com>
2025-03-22 22:23:33 +0900

    feat: H-index 1차 시도 (3. citations 내림차순 정렬 및 int 배열로 환원)

--- src/programmers/_42747/Solution.java
+++ src/programmers/_42747/Solution.java
@@ -1,12 +1,15 @@
 package programmers._42747;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
 
-        Arrays.stream(citations).boxed();
+        citations = Arrays.stream(citations).boxed()
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(i -> i).toArray();
 
         return answer;
     }
