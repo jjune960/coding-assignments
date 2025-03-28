2209a3093c82740e0140e2b194c57d4ccacecb90
jjune960 <jjune960@gmail.com>
2025-03-28 23:03:08 +0900

    feat: 기능개발 1차 시도 (1. 모든 원소를 speeds 만큼 더하기)

--- src/programmers/_42586/Solution.java
+++ src/programmers/_42586/Solution.java
@@ -1,8 +1,14 @@
 package programmers._42586;
 
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
+
+        for (int i = 0; i < progresses.length; i++) {
+            progresses[i] += speeds[i];
+        }
+
         return answer;
     }
 }
