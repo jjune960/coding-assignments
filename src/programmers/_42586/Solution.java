aeb5f7bff399b7f45cb1ab394cb771bb2488f9b0
jjune960 <jjune960@gmail.com>
2025-03-29 18:51:49 +0900

    feat: 기능개발 1차 시도 (3. 결과를 answer에 넣기)

--- src/programmers/_42586/Solution.java
+++ src/programmers/_42586/Solution.java
@@ -1,27 +1,35 @@
 package programmers._42586;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         int[] answer = {};
+        ArrayList<Integer> answerList = new ArrayList<>();
+
+        //int answerindex = 0;
 
         for (int i = 0; i < progresses.length; i++) {
             progresses[i] += speeds[i];
         }
 
         if(progresses[0] >= 100) {
-            System.out.println("over 100%");
+            answerList.add(1);
         }
 
-        return progresses;
-        //return answer;
+        System.out.println(Arrays.toString(progresses));
+
+        answer = answerList.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
 
-        String result = Arrays.toString(sol.solution(new int[]{99, 30, 55}, new int[]{1, 30, 5}));
+        String result = Arrays.toString(sol.solution
+                (new int[]{99, 30, 55}, new int[]{1, 30, 5}));
 
         System.out.println(result);
     }
