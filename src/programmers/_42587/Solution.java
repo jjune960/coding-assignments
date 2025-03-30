1829b02eddea9e00d09dfe3d80903459944988b3
jjune960 <jjune960@gmail.com>
2025-03-30 19:51:10 +0900

    feat: 프로세스 1차 시도 (1. Queue 만들기)

--- src/programmers/_42587/Solution.java
+++ src/programmers/_42587/Solution.java
@@ -1,8 +1,14 @@
 package programmers._42587;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
+
+        Deque<Integer> priortyqueue = new ArrayDeque<>();
+
         return answer;
     }
 }
