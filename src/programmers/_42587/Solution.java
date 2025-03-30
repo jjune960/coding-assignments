e3dffaa8aa8b81ea3b9dec957fefb8e0d6b57b07
jjune960 <jjune960@gmail.com>
2025-03-30 22:54:12 +0900

    docs: 프로세스 2차 시도 (2. 현재 문서보다 우선순위가 높은 것이 있는지 확인)

--- src/programmers/_42587/Solution.java
+++ src/programmers/_42587/Solution.java
@@ -10,7 +10,20 @@ class Solution {
 
         Deque<Integer> processDeque = new ArrayDeque<>();
         for (int i = 0; i < priorities.length; i++) {
-            processDeque.offer(i);
+            processDeque.addLast(i);
+        }
+
+        while (!processDeque.isEmpty()) {
+            int current = processDeque.removeFirst();
+            boolean isHighest = true;
+
+            // 남은 문서들 중 현재 문서보다 우선순위가 높은 것이 있는지 확인
+            for (int index : processDeque) {
+                if (priorities[index] > priorities[current]) {
+                    isHighest = false;
+                    break;
+                }
+            }
         }
 
         return answer;
