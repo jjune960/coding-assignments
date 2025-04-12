b6f145550bb6c57a85744b65b7e9aa27db224721
jjune960 <jjune960@gmail.com>
2025-04-12 17:39:17 +0900

    feat: Solution.java _1655,1715 add

---
+++ src/baekjoon/_1655/Solution.java
@@ -0,0 +1,14 @@
+package baekjoon._1655;
+
+public class Solution {
+
+    public String solution(int[] numbers) {
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/baekjoon/_1715/Solution.java
@@ -0,0 +1,30 @@
+package baekjoon._1715;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Solution {
+    PriorityQueue<Integer>  = new PriorityQueue<>();
+
+    public String solution(int[] numbers) {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        var tokenizer = new StringTokenizer(reader.readLine());
+        int N = Integer.parseInt(tokenizer.nextToken());
+        int[] M = new int[N];
+        for(int i = 0; i < N; i++)
+            M[i] = Integer.parseInt(tokenizer.nextToken());
+
+        PriorityQueue
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
