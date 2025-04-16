45bcaf3be0619a95fbf8754f67c867a1ae4349c0
jjune960 <jjune960@gmail.com>
2025-04-16 16:06:52 +0900

    feat: 카드 정렬하기 2차 시도 (실패)

--- src/baekjoon/_1715/Solution.java
+++ src/baekjoon/_1715/Solution.java
@@ -10,18 +10,22 @@ class Solution {
     public static void main(String[] args) throws IOException {
         PriorityQueue<Integer> cards = new PriorityQueue<>();
         int sum = 0;
-        int previous = 0;
 
         var reader = new BufferedReader(new InputStreamReader(System.in));
         var tokenizer = new StringTokenizer(reader.readLine());
         int N = Integer.parseInt(tokenizer.nextToken());
         //int[] M = new int[N];
-        for(int i = 0; i < N; i++)
+        for(int i = 0; i < N; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
             cards.add(Integer.parseInt(tokenizer.nextToken()));
+        }
 
         for(int i = 0; i < N; i++) {
             int now = cards.remove();
-            sum += (previous + now);
+            if (i == 1)
+                sum = (sum + now);
+            else
+                sum += (sum + now);
         }
 
         System.out.println(sum);
