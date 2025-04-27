1b56aaa66c5fc1d51e6e547a5857473f1b7db1f4
jjune960 <jjune960@gmail.com>
2025-04-27 20:29:24 +0900

    feat: 다리 놓기 1차 시도 (실패)

---
+++ src/baekjoon/_1010/Main.java
@@ -0,0 +1,38 @@
+package baekjoon._1010;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main  {
+    public static void main(String[] args) throws IOException {
+        var reader = new BufferedReader(new InputStreamReader(System.in));
+        var tokenizer = new StringTokenizer(reader.readLine());
+        int count = Integer.parseInt(tokenizer.nextToken());
+        double result = 0.0;
+        int n[] = new int[count];
+        int r[] = new int[count];
+        int tempn;
+        int tempnminr;
+
+        for(int i = 0; i < count; i++) { // 다리 개수 입력 (
+            tokenizer = new StringTokenizer(reader.readLine());
+            n[i] = Integer.parseInt(tokenizer.nextToken());
+            r[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        for(int i = 0; i < count; i++) {
+            tempn = n[i];
+            tempnminr = n[i] - r[i];
+
+            for(int j = tempn; j > 1; j--) {
+                tempn *= n[i];
+            }
+            for(int k = tempnminr; k > 1; k--) {
+                tempnminr *= k;
+            }
+            System.out.println(tempn/tempnminr);
+        }
+    }
+}
--- src/programmers/_62048/Solution.java
+++ src/programmers/_62048/Solution.java
@@ -4,8 +4,8 @@ class Solution {
     public long solution(int w, int h) {
         long answer = 1;
 
+        
 
-
-        return answer;
+        return answer; 
     }
 }
